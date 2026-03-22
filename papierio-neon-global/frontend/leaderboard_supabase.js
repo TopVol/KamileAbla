d4640d3ffff4aa4c212798bf4b13eb4d8e66f8f8
TopVol <kulki@web.de>
2026-03-23 00:05:38 +0100

    Add Supabase leaderboard config

---
+++ papierio-neon-global/frontend/leaderboard_supabase.js
@@ -0,0 +1,5 @@
+window.LEADERBOARD_CONFIG = {
+  supabaseUrl: 'https://ampkgrgbcwabnbsrtfwe.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtcGtncmdiY3dhYm5ic3J0ZndlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyMTk5MjAsImV4cCI6MjA4OTc5NTkyMH0.c_8isT6sQSPV2J3Fm-naD6wEWGOSPON40pfWlaxiDN8',
+  tableName: 'leaderboard_scores'
+};
