090a44afbd2f284f10379cbc52cfdf807c1ee93f
TopVol <kulki@web.de>
2026-03-23 09:09:34 +0100

    Update Supabase anon key in leaderboard config

--- papierio-neon-global/frontend/leaderboard_supabase.js
+++ papierio-neon-global/frontend/leaderboard_supabase.js
@@ -1,5 +1,5 @@
 window.LEADERBOARD_CONFIG = {
   supabaseUrl: 'https://ampkgrgbcwabnbsrtfwe.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtcGtncmdiY3dhYm5ic3J0ZndlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyMTk5MjAsImV4cCI6MjA4OTc5NTkyMH0.c_8isT6sQSPV2J3Fm-naD6wEWGOSPON40pfWlaxiDN8',
+  supabaseAnonKey: 'sb_publishable_2XYoSTfD5ZUvFhTcbDClTA_h3l5yPf-',
   tableName: 'leaderboard_scores'
 };
